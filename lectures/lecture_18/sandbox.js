198fd8b29d42863945b5057f9ffc514ee3453cf4
Charlie Lindgren <clg@du.se>
2024-09-20 11:35:41 +0200

    Refactor lecture 18 JavaScript file and demonstrate the use of a separate callback function in forEach loop

--- lectures/lecture_18/sandbox.js
+++ lectures/lecture_18/sandbox.js
@@ -24,6 +24,26 @@ numbers.forEach(function(number) {
 // the callback for each element in the array. In this case, the callback function logs
 // each number to the console.
 
+// Let us make it even more clear that a callback function is used in the forEach method
+// by defining the callback function separately and passing it as an argument to the forEach
+// method.
+
+// Define a callback function that logs a number to the console
+function logNumber(number) {
+  console.log(number);
+}
+
+// Use the forEach method with the logNumber callback function
+numbers.forEach(logNumber);
+
+// In this example, we define a callback function called logNumber that logs a number to
+// the console. We then use the forEach method with the logNumber callback function to
+// iterate over the array of numbers and log each number to the console.
+
+// Note that we did not need to pass arguments to the logNumber function when using it as
+// a callback function in the forEach method. The forEach method automatically passes the
+// current element of the array as an argument to the callback function.
+
 // The forEach method is a powerful tool for iterating over arrays in JavaScript. It provides
 // a concise and expressive way to perform operations on each element of an array.
 
